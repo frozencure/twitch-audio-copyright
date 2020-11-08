d2d55e2afb11a513c4d32e0d096953cd0bbfe1f1
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-11-08 22:20:21 +0100

    login layout

--- apps/client/src/app/login/login.module.ts
+++ apps/client/src/app/login/login.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [ LoginComponent ],
   exports: [ LoginComponent ],
   imports: [
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class LoginModule {
