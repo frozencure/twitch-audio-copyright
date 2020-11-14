5976fde5e0ff501b0f261458bd73fb3cdb01232c
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-11-14 21:55:38 +0100

    feat: why component

--- apps/landing-page/src/app/home/home.module.ts
+++ apps/landing-page/src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HeroModule } from '../components/hero/hero.module';
 import { ServicesModules } from '../components/services/services.modules';
+import { WhyModule } from '../components/why/why.module';
 
 @NgModule({
   declarations: [ HomeComponent ],
   imports: [
     HeroModule,
-    ServicesModules
+    ServicesModules,
+    WhyModule
   ],
   exports: [ HomeComponent ]
 })
