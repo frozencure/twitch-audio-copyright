2e72b92ea1d2d8b55d5081462c7d098660ebc6fc
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-11-14 15:54:50 +0100

    add bootstrap to landing-page

--- apps/landing-page/src/app/app.module.ts
+++ apps/landing-page/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
