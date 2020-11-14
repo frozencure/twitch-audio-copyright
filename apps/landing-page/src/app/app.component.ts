a75c52ec67a5faf17b1af2fb784ba4b4ede5317b
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-11-14 14:30:41 +0100

    add landingPage

---
+++ apps/landing-page/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitch-audio-copyright-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'landing-page';
+}
