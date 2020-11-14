1f3195fb1c5aef506ea6bccfe57630e94acd9535
Andy <eduard-andrei.radulescu@bawagpsk.com>
2020-11-14 17:54:30 +0100

    add navbar component

--- apps/landing-page/src/app/app.module.ts
+++ apps/landing-page/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarModule } from './components/navbar/navbar.module';
 
 @NgModule({
   declarations: [ AppComponent ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NavbarModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
