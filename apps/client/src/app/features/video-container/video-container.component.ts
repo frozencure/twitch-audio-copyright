7ac758482c073a525593ae4bf751cb8a7c87ed3c
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2020-11-24 10:23:22 +0100

    implement data table

--- apps/client/src/app/features/video-container/video-container.component.ts
+++ apps/client/src/app/features/video-container/video-container.component.ts
@@ -6,19 +6,23 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-video-container',
   templateUrl: './video-container.component.html',
-  styleUrls: [ './video-container.component.scss' ]
+  styleUrls: ['./video-container.component.scss']
 })
 export class VideoContainerComponent implements OnInit {
   public videos$: Observable<Video[]>;
   public type: string;
+  public isLoadingResults: boolean;
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
 
   constructor(private actRoute: ActivatedRoute) {
+    this.isLoadingResults = false;
   }
 
   ngOnInit(): void {
     this.actRoute.data.subscribe(data => {
       this.type = data.routeResolver.type;
       this.videos$ = data.routeResolver.stream;
+      this.isLoadingResults = true;
     }, err => console.log(err));
   }
 
--- apps/client/src/app/features/video-container/video-container.module.ts
+++ apps/client/src/app/features/video-container/video-container.module.ts
@@ -4,13 +4,19 @@ import { VideoResolver } from './video.resolver.service';
 import { VideoItemModule } from '../video-item/video-item.module';
 import { CommonModule } from '@angular/common';
 import { ClipItemModule } from '../clip-item/clip-item.module';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [ VideoContainerComponent ],
   imports: [
     CommonModule,
     VideoItemModule,
-    ClipItemModule
+    ClipItemModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatTableModule
   ],
   exports: [ VideoContainerComponent ],
   providers: [ VideoResolver ]
